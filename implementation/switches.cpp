c5fa154b974306f160e3664234f2b65c99528266
julkiewicz <julkiewicz@e2cb09e9-51e2-4e97-b92c-ac25115bd917>
2010-03-14 18:33:03 +0000

    Dodanie przełączników na potrzeby PATH-RAVE/AMAF.
    git-svn-id: http://svn2.assembla.com/svn/mimhex@55 e2cb09e9-51e2-4e97-b92c-ac25115bd917

--- implementation/switches.cpp
+++ implementation/switches.cpp
@@ -8,5 +8,9 @@ SWITCH_TYPE Switches::patternsOn = true;
 SWITCH_TYPE Switches::detectWins = true;
 SWITCH_TYPE Switches::raveOn = false;
 SWITCH_TYPE Switches::pathAmafOn = false;
+SWITCH_TYPE Switches::pathRaveOn = false;
+SWITCH_TYPE Switches::floodFillOn = false;
+SWITCH_TYPE Switches::singleShortestOn = false;
+SWITCH_TYPE Switches::allShortestOn = false;
 
 }
--- implementation/switches.h
+++ implementation/switches.h
@@ -32,36 +32,80 @@ class Switches {
             return detectWins;
         }
 
-        static bool PathAmaf() {
-            return pathAmafOn;
-        }
-
         static bool Rave() {
             return raveOn;
         }
 
+        static bool PathAmaf() {
+            return pathAmafOn;
+        }
+
+        static bool PathRave() {
+            return pathRaveOn;
+        }
+
+        static bool FloodFill() {
+            return floodFillOn;
+        }
+
+        static bool SingleShortest() {
+            return singleShortestOn;
+        }
+
+        static bool AllShortest() {
+            return allShortestOn;
+        }
+
         static void SetAvoidingBridges(bool b) {
-            SetSwitch("avoidBridges", avoidingBridgesOn, b);
+            SetSwitch("Bridge avoiding", avoidingBridgesOn, b);
         }
 
         static void SetDefendingBridges(bool b) {
-            SetSwitch("defendBridges", defendingBridgesOn, b);
+            SetSwitch("Bridge defending", defendingBridgesOn, b);
         }
 
         static void SetPatternsUsed(bool b) {
-            SetSwitch("patternsUsed", patternsOn, b);
+            SetSwitch("Pattern use", patternsOn, b);
         }
 
         static void SetDetectWins(bool b) {
-            SetSwitch("detectWins", detectWins, b);
+            SetSwitch("Early win detection", detectWins, b);
         }
 
         static void SetRave(bool b) {
-            SetSwitch("RaveOn", raveOn, b);
+            SetSwitch("RAVE", raveOn, b);
         }
 
         static void SetPathAmaf(bool b) {
-            SetSwitch("pathAmafOn", pathAmafOn, b);
+            SetSwitch("Path AMAF", pathAmafOn, b);
+        }
+
+        static void SetPathRave(bool b) {
+            SetSwitch("Path RAVE", pathRaveOn, b);
+        }
+
+        static void SetFloodFill(bool b) {
+            SetSwitch("Flood-fill strategy", floodFillOn, b);
+            if (b) {
+                SetSwitch("Single shortest strategy", singleShortestOn, false);
+                SetSwitch("All shortest paths strategy", allShortestOn, false);
+            }
+        }
+
+        static void SetSingleShortest(bool b) {
+            SetSwitch("Single shortest path strategy", singleShortestOn, b);
+            if (b) {
+                SetSwitch("Flood-fill strategy", floodFillOn, false);
+                SetSwitch("All shortest paths strategy", allShortestOn, false);
+            }
+        }
+
+        static void SetAllShortest(bool b) {
+            SetSwitch("All shortest paths strategy", allShortestOn, b);
+            if (b) {
+                SetSwitch("Flood-fill strategy", floodFillOn, false);
+                SetSwitch("Single shortest path strategy", singleShortestOn, false);
+            }
         }
 
     private:
@@ -71,8 +115,13 @@ class Switches {
         static SWITCH_TYPE detectWins;
         static SWITCH_TYPE raveOn;
         static SWITCH_TYPE pathAmafOn;
+        static SWITCH_TYPE pathRaveOn;
+        static SWITCH_TYPE floodFillOn;
+        static SWITCH_TYPE singleShortestOn;
+        static SWITCH_TYPE allShortestOn;
 
         static void SetSwitch(const std::string& name, bool& value, bool newValue) {
+
 #ifdef DYNAMIC_SWITCHES
             std::cerr << "INFO: Switched `" << name << "` to " << int(newValue) << std::endl;
             value = newValue;
