8446b13a53b5e03f035de4205379ea25b98ac9e4
Krzysztof Stefański <>
2010-03-20 16:37:32 +0100

    dodanie brakujacego pliku

---
+++ patterns/rep.h
@@ -0,0 +1,9 @@
+#ifndef REP_H_
+#define REP_H_
+
+#include "rep.h"
+
+#define rep(iterator, upper_bound)                           \
+        for (uint iterator = 0; iterator < upper_bound; ++iterator)
+
+#endif /* REP_H_ */
