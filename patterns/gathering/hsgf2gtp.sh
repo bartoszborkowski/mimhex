d3e8b60c374d28689eca924db7c2155a2c20a20c
Bartosz Borkowski <bb248235@students.mimuw.edu.pl>
2010-04-07 21:00:23 +0200

    Implemented swap rule into pattern collection.

--- patterns/gathering/hsgf2gtp.sh
+++ patterns/gathering/hsgf2gtp.sh
@@ -3,28 +3,29 @@
 convert() 
 {
 	file=$1
-    echo "newgame"
 	{ cat $file | sed "s/;/\n/g"  | grep -v "^($" | grep -v "FF\|SO" | grep -v "resign" \
    		| sed "s/W/play white/" | sed "s/B/play black/" | grep "swap" > /dev/null && { 
 			cat $file | sed "s/;/\n/g"  | grep -v "^($" | grep -v "FF\|SO" | grep -v "resign" \
-		   		| sed "s/W/play white/" | sed "s/B/play black/" | grep -v "swap" | sed "1s/white/black/"; 
+		   		| sed "s/W/play white/" | sed "s/B/play black/" |  sed "2s/.*swap.*/swap/" \
+#                | sed "4~2s/black/white/" | sed "3~2s/white/black/"\
+ #               ; \
 		} || {
 			cat $file | sed "s/;/\n/g"  | grep -v "^($" | grep -v "FF\|SO" | grep -v "resign" \
-		   		| sed "s/W/play white/" | sed "s/B/play black/"; } ;} | sed "s/\[\(.\)\(.\)\]/ \1 \2/" | gawk '{\
-		c=$4;
-		ascval = b = 128; \
-		while ((tchar = sprintf("%c", ascval)) != c) \
-		ascval += (b /= 2) * (tchar < c) ? 1 : -1; printf ("%s %s %s%d\n", $1, $2, $3, ascval-96)}'
+		   		| sed "s/W/play white/" | sed "s/B/play black/"; } ;} | sed "s/\[\(.\)\(.\)\]/ \1 \2/" \
+                | gawk '{\
+                if ($1 != "swap") {\
+                    c=$4;
+                    ascval = b = 128; \
+                    while ((tchar = sprintf("%c", ascval)) != c) \
+                        ascval += (b /= 2) * (tchar < c) ? 1 : -1; \
+                        printf ("%s %s %s%d\n", $1, $2, $3, ascval-96) \
+                    } else {print "swap"}\
+
+    }'
+
 }
 
 #cat games/1022780.sgf | sed "s/;/\n/g"  | grep -v "^($" | grep -v "FF\|SO" | grep -v "resign" \
 #	| sed "s/W/play white/" | sed "s/B/play black/" | sed "s/\[\(.\)\(.\)\]/ \1 \2/" | sed "1s/white/black/"  | grep -v swap 
 
-test -n "$1" || { echo "usage: ./hsgf2gtp.sh sgfPath1 sgfPath2 ... " && exit 1; }
-
-while test -n "$1" ; do
-    convert $1
-    shift
-done
-
-echo "print"
+convert $1
